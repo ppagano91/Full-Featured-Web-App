93dd7ded7e38d06ccd2775fccadab4758f8009d7
Patricio Pagano <patricio.pagano@paradigma.com.ar>
2022-03-29 09:54:54 -0300

    nuevas funcionalidades en ruta /account

--- Flask_blog/flaskblog/routes.py
+++ Flask_blog/flaskblog/routes.py
@@ -1,6 +1,10 @@
+import os
+import secrets
+
+from PIL import Image
 from flask import Flask, render_template,url_for,flash,redirect,request
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models.models import User,Post
 
 # For Login
@@ -70,8 +74,39 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    # No se guarda el nombre original de la imagen porque podría suceder que ya existiera ese nombre
+    random_hex = secrets.token_hex(8)
+    f_name, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path=os.path.join(app.root_path, 'static/profile_pics', picture_fn)
 
-@app.route("/account")
+    # Resizing image to 125x125 px
+    output_size = (125,125)
+    i=Image.open(form_picture)
+    i.thumbnail(output_size)
+
+
+    i.save(picture_path)
+    return picture_fn
+
+@app.route("/account", methods=["POST","GET"])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file=save_picture(form.picture.data)
+            current_user.image_file=picture_file
+
+        current_user.username=form.username.data
+        current_user.email=form.email.data
+        db.session.commit()
+        flash("Your account has been updated!","success")
+        return redirect(url_for('account'))
+    elif request.method=='GET':
+        form.username.data=current_user.username
+        form.email.data=current_user.email
+    image_file=url_for('static', filename='profile_pics/'+current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
