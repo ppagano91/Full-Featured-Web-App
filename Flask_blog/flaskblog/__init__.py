b906f6f7618608c75509992fc4d32dd0419ed417
Patricio Pagano <patricio.pagano@paradigma.com.ar>
2022-04-14 09:56:07 -0300

    pip install Mail y configuración

--- Flask_blog/flaskblog/__init__.py
+++ Flask_blog/flaskblog/__init__.py
@@ -1,7 +1,11 @@
+from dotenv import load_dotenv
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+
 # import sqlalchemy
 
 # import secrets
@@ -32,6 +36,19 @@ login_manager = LoginManager(app)
 # login_manager.login_view='<function name of a route>'
 login_manager.login_view='login'
 login_manager.login_message_category='info'
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+
+# ENVIROMENT VARIABLES: https://www.youtube.com/watch?v=IolxqkL7cD8
+load_dotenv()
+app.config['MAIL_USERNAME'] = os.environ['EMAIL_USER']
+app.config['MAIL_PASSWORD'] = os.environ['EMAIL_PASSWORD']
+
+mail=Mail(app)
+
+
 
 # db.create_engine('postgresql+psycopg2://postgres@localhost/db_flaskblog',{})
 # Crear una instancia del base de datos
