fe7123f821ecded529454dccc3b990ea10108020
Patricio Pagano <patricio.pagano@paradigma.com.ar>
2022-06-01 12:10:38 -0300

    fix - add @errors.app_errorhandler

--- Flask_blog/flaskblog/errors/handlers.py
+++ Flask_blog/flaskblog/errors/handlers.py
@@ -8,8 +8,10 @@ def error_404(error):
     # 404 is a static code
     return render_template('errors/404.html'), 404
 
+@errors.app_errorhandler(403)
 def error_403(error):
     return render_template('errors/403.html'), 403
 
+@errors.app_errorhandler(500)
 def error_500(error):
     return render_template('errors/500.html'), 500
