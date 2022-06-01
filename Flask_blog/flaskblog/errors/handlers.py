10e553404794388a1aa8db5ec52d7836356103e2
Patricio Pagano <patricio.pagano@paradigma.com.ar>
2022-06-01 11:28:57 -0300

    templates has been created and registered

--- Flask_blog/flaskblog/errors/handlers.py
+++ Flask_blog/flaskblog/errors/handlers.py
@@ -1,4 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 errors= Blueprint('errors',__name__)
 
+# Para manejar el error 404
+@errors.app_errorhandler(404)
+def error_404(error):
+    # 404 is a static code
+    return render_template('errors/404.html'), 404
+
+def error_403(error):
+    return render_template('errors/403.html'), 403
+
+def error_500(error):
+    return render_template('errors/500.html'), 500
